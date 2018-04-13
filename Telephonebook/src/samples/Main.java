ab91b223a2addbc80782af086716251ed343a52d
Jack-Music <theonlymusicjack@gmail.com>
2018-04-13 16:28:55 +0200

    Updated Files

--- Telephonebook/src/samples/Main.java
+++ Telephonebook/src/samples/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -21,98 +22,98 @@ import javafx.stage.Stage;
 import ui.EntryArea;
 
 public class Main extends Application {
-static final Path path = Paths.get("C:/Users/geiers/Desktop/test.json");
+    static final Path path = Paths.get("src/telefonbook.json");
 
-private TelefonBook telefonBook = new TelefonBook();
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		BorderPane root = new BorderPane();
-		
+    private TelefonBook telefonBook = new TelefonBook();
 
-	
-		EntryArea entryArea = new EntryArea(telefonBook.getNumbers());
-		SearchArea searchArea = new SearchArea(searchText -> {
-			telefonBook.search(searchText);
-			return null;
-		});
-		DeleteArea deleteArea = new DeleteArea(() -> {
-			telefonBook.removeAll(entryArea.getSelectedEntries());
-			return null;
-		}, () -> {
-			telefonBook.createEntry(new TelefonEntry());
-			return null;
-		}, () -> {
-			JsonFactory factory = new JsonFactory();
-			try (OutputStream os = Files.newOutputStream(path); JsonGenerator jg = factory.createGenerator(os)) {
-				// Verwenden Sie jg um fuer jeden Eintrag im Telefonbuch
-				// entsprechende Objekte im JSON zu erzeugen
-				jg.writeStartArray();
-				for (TelefonEntry e : telefonBook.getAllEntrysAsArray()) {
-					jg.writeStartObject();
-					jg.writeStringField("LastName", e.getLastName());
-					jg.writeStringField("FirstName", e.getFirstName());
-					jg.writeStringField("Number", e.getNumber());
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane root = new BorderPane();
 
-					jg.writeEndObject();
-				}
-				jg.writeEndArray();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return null;
-		});
+        EntryArea entryArea = new EntryArea(telefonBook.getNumbers());
+        SearchArea searchArea = new SearchArea(searchText -> {
+            telefonBook.search(searchText);
+            return null;
+        });
+        DeleteArea deleteArea = new DeleteArea(() -> {
+            telefonBook.removeAll(entryArea.getSelectedEntries());
+            return null;
+        }, () -> {
+            telefonBook.createEntry(new TelefonEntry());
+            return null;
+        }, () -> {
+            JsonFactory factory = new JsonFactory();
+            try (OutputStream os = Files.newOutputStream(path); JsonGenerator jg = factory.createGenerator(os)) {
+                // Verwenden Sie jg um fuer jeden Eintrag im Telefonbuch
+                // entsprechende Objekte im JSON zu erzeugen
+                jg.writeStartArray();
+                for (TelefonEntry e : telefonBook.getAllEntrysAsArray()) {
+                    jg.writeStartObject();
+                    jg.writeStringField("LastName", e.getLastName());
+                    jg.writeStringField("FirstName", e.getFirstName());
+                    jg.writeStringField("Number", e.getNumber());
 
-		root.setTop(searchArea.getPane());
-		root.setBottom(deleteArea.getPane());
-		root.setCenter(entryArea.getAnchorPane());
+                    jg.writeEndObject();
+                }
+                jg.writeEndArray();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
 
-		//LOAD
-		
-		try (InputStream is = Files.newInputStream(path)) {
-			ObjectMapper mapper = new ObjectMapper();
-			JsonNode roots = mapper.readTree(is);
-			// Verwenden Sie den ObjectMapper hier , um
-			// die Daten aus der Datei auszulesen.
-			for (JsonNode j : roots) {
-				telefonBook.createEntry(new TelefonEntry(j.path("LastName").asText(), j.path("FirstName").asText(),
-						j.path("Number").asText()));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		primaryStage.setTitle("TelephoneBook");
-		primaryStage.setScene(new Scene(root, 500, 600));
-		primaryStage.show();
-	}
-	
-	
-	@Override
-	public void stop() throws Exception {
-		
-		//SAVE
-		
-		JsonFactory factory = new JsonFactory();
-		try (OutputStream os = Files.newOutputStream(path); JsonGenerator jg = factory.createGenerator(os)) {
-			// Verwenden Sie jg um fuer jeden Eintrag im Telefonbuch
-			// entsprechende Objekte im JSON zu erzeugen
-			jg.writeStartArray();
-			for (TelefonEntry e : telefonBook.getAllEntrysAsArray()) {
-				jg.writeStartObject();
-				jg.writeStringField("LastName", e.getLastName());
-				jg.writeStringField("FirstName", e.getFirstName());
-				jg.writeStringField("Number", e.getNumber());
+        root.setTop(searchArea.getPane());
+        root.setBottom(deleteArea.getPane());
+        root.setCenter(entryArea.getAnchorPane());
 
-				jg.writeEndObject();
-			}
-			jg.writeEndArray();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		super.stop();
-	}
-	
+        // LOAD
+        if (!Files.exists(path, LinkOption.NOFOLLOW_LINKS))
+            Files.createFile(path);
+        else
+            try (InputStream is = Files.newInputStream(path)) {
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode roots = mapper.readTree(is);
+                // Verwenden Sie den ObjectMapper hier , um
+                // die Daten aus der Datei auszulesen.
+                for (JsonNode j : roots) {
+                    telefonBook.createEntry(new TelefonEntry(j.path("LastName").asText(), j.path("FirstName").asText(),
+                            j.path("Number").asText()));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        primaryStage.setTitle("TelephoneBook");
+        primaryStage.setScene(new Scene(root, 500, 600));
+        primaryStage.show();
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void stop() throws Exception {
+
+        // SAVE
+
+        JsonFactory factory = new JsonFactory();
+        try (OutputStream os = Files.newOutputStream(path); JsonGenerator jg = factory.createGenerator(os)) {
+            // Verwenden Sie jg um fuer jeden Eintrag im Telefonbuch
+            // entsprechende Objekte im JSON zu erzeugen
+            jg.writeStartArray();
+            for (TelefonEntry e : telefonBook.getAllEntrysAsArray()) {
+                jg.writeStartObject();
+                jg.writeStringField("LastName", e.getLastName());
+                jg.writeStringField("FirstName", e.getFirstName());
+                jg.writeStringField("Number", e.getNumber());
+
+                jg.writeEndObject();
+            }
+            jg.writeEndArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        super.stop();
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
 }
